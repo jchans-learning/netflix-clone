dd9b56fa0d0d69f0095495df5682c23b380fcfe8
jchans <hanschiang86@gmail.com>
2021-10-08 19:40:52 +0800

    Add Row.js to create Row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Row from "./Row";
 
 function App() {
   return (
     <div className="App">
       <h1>Let's Build a Netflix Clone Today!</h1>
+      <Row title="NETFLIX ORIGIANLS" />
+      <Row title="Trending Now" />
     </div>
   );
 }
