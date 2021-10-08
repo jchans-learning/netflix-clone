91e1aa1489838deb2b028f83a928fe7b11d66cf6
jchans <hanschiang86@gmail.com>
2021-10-09 04:11:53 +0800

    Get data from API setting

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 
-function Row({ title }) {
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  // console.log(movies);
+
   return (
     <div>
       <h2>{title}</h2>
-
-      {/* container -> posters */}
+      <div className="row__posters ">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            src={`${baseUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
