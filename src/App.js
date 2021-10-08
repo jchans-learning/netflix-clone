9f5ab691d9509739d26272df680d176459ca8110
jchans <hanschiang86@gmail.com>
2021-10-09 04:10:14 +0800

    Import request and set Row props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Row from "./Row";
+import requests from "./requests.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Let's Build a Netflix Clone Today!</h1>
-      <Row title="NETFLIX ORIGIANLS" />
-      <Row title="Trending Now" />
+      <Row
+        title="NETFLIX ORIGIANLS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
